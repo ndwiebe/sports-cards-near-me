8d0093e27bdaef7a14240d441afaa7f063de6bf6
ndwiebe <dominathan@gmail.com>
2025-06-01 22:06:10 -0600

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-import { initMap } from './map.js';
-import { loadStoresFromSheet } from './loadStores.js';
-
-window.initMap = async () => {
-  const stores = await loadStoresFromSheet();
-  initMap(stores);
-};
